36710430e9e940ba81af179839bd3957ccb2127c
EC2 Default User <ec2-user@ip-172-31-13-11.us-east-2.compute.internal>
2018-06-11 10:07:02 +0000

    Finish sandbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tasks
   get 'helo/index'
   get 'helo', to: 'helo#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_114001) do
+ActiveRecord::Schema.define(version: 2018_06_11_090242) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "introduction"
     t.integer "age"
     t.boolean "important"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "detaile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
